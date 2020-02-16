11813b4b061ca561de7bd4f429abe2e229ab4231
Dan Booth <DanBoothDev@gmail.com>
2020-02-16 13:55:38 -0400

    Added offline page to pre-cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,14 +20,20 @@
 // CODELAB: Update cache names any time any of the cached files change.
 const CACHE_NAME = 'static-cache-v1';
 
-// CODELAB: Add list of files to cache here.
+// list of files to cache
 const FILES_TO_CACHE = [
+  '/offline.html',
 ];
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
-
+  // Precache static resources
+  evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log('[ServiceWorker] Pre-caching offline page');
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
   self.skipWaiting();
 });
 
