0a4986ebb7f932a8303d46f37232bc84f07947dc
Dan Booth <DanBoothDev@gmail.com>
2020-02-16 14:14:35 -0400

    Added failed network request handler

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,7 +18,7 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v2';
+const CACHE_NAME = 'static-cache-v1';
 
 // list of files to cache
 const FILES_TO_CACHE = [
@@ -55,6 +55,19 @@ self.addEventListener('activate', (evt) => {
 
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
+  //fetch event handler
+  if (evt.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return;
+  }
+  evt.respondWith(
+      fetch(evt.request)
+          .catch(() => {
+            return caches.open(CACHE_NAME)
+                .then((cache) => {
+                  return cache.match('offline.html');
+                });
+          })
+  );
 
 });
