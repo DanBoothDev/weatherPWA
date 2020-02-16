265baaaa5f7b284e7bb9be77732018dc171dbe5f
Dan Booth <DanBoothDev@gmail.com>
2020-02-16 14:04:49 -0400

    Removed cached data on activate

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,7 +18,7 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v2';
 
 // list of files to cache
 const FILES_TO_CACHE = [
@@ -39,8 +39,17 @@ self.addEventListener('install', (evt) => {
 
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
-
+  // Remove previous cached data from disk
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
   self.clients.claim();
 });
 
